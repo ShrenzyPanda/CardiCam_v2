71edfa961fe1d32c74ebc38436faf48449ce6b17
heisenbug237 <areebsyed237@gmail.com>
2023-02-07 18:33:46 +0530

    #4

--- datasets/test_dataset.py
+++ datasets/test_dataset.py
@@ -1,28 +1,85 @@
-from torch.utils.data import Dataset
-from PIL import Image
-import os
+import os, glob
+from pathlib import Path
+import numpy as np
+import cv2
 
 
-class ScreenDataset(Dataset):
-    def __init__(self, data_path, transform=None, is_dir=True):
-        self.transform = transform
-        self.is_dir = is_dir
-        if not is_dir:
-            self.image_path = data_path
-            self.image = Image.open(self.image_path)
+# acceptable image suffixes
+img_formats = ['bmp', 'jpg', 'jpeg', 'png', 'tif', 'tiff', 'dng', 'webp', 'mpo'] 
+
+
+def letterbox(img, new_shape=(640, 640), color=(114, 114, 114), auto=True, scaleFill=False, scaleup=True, stride=32):
+    # Resize and pad image while meeting stride-multiple constraints
+    shape = img.shape[:2]  # current shape [height, width]
+    if isinstance(new_shape, int):
+        new_shape = (new_shape, new_shape)
+
+    # Scale ratio (new / old)
+    r = min(new_shape[0] / shape[0], new_shape[1] / shape[1])
+    if not scaleup:  # only scale down, do not scale up (for better test mAP)
+        r = min(r, 1.0)
+
+    # Compute padding
+    ratio = r, r  # width, height ratios
+    new_unpad = int(round(shape[1] * r)), int(round(shape[0] * r))
+    dw, dh = new_shape[1] - new_unpad[0], new_shape[0] - new_unpad[1]  # wh padding
+    if auto:  # minimum rectangle
+        dw, dh = np.mod(dw, stride), np.mod(dh, stride)  # wh padding
+    elif scaleFill:  # stretch
+        dw, dh = 0.0, 0.0
+        new_unpad = (new_shape[1], new_shape[0])
+        ratio = new_shape[1] / shape[1], new_shape[0] / shape[0]  # width, height ratios
+
+    dw /= 2  # divide padding into 2 sides
+    dh /= 2
+
+    if shape[::-1] != new_unpad:  # resize
+        img = cv2.resize(img, new_unpad, interpolation=cv2.INTER_LINEAR)
+    top, bottom = int(round(dh - 0.1)), int(round(dh + 0.1))
+    left, right = int(round(dw - 0.1)), int(round(dw + 0.1))
+    img = cv2.copyMakeBorder(img, top, bottom, left, right, cv2.BORDER_CONSTANT, value=color)  # add border
+    return img, ratio, (dw, dh)
+
+class LoadImages:  # for inference
+    def __init__(self, path, img_size=640, stride=32):
+        p = str(Path(path).absolute())  # os-agnostic absolute path
+        if '*' in p:
+            files = sorted(glob.glob(p, recursive=True))  # glob
+        elif os.path.isdir(p):
+            files = sorted(glob.glob(os.path.join(p, '*.*')))  # dir
+        elif os.path.isfile(p):
+            files = [p]  # files
         else:
-            self.folder_path = data_path
-            self.image_paths = [os.path.join(self.folder_path, image_name) 
-                for image_name in os.listdir(self.folder_path)]
+            raise Exception(f'ERROR: {p} does not exist')
+
+        images = [x for x in files if x.split('.')[-1].lower() in img_formats]
+        self.nf = len(images) # number of files
+        self.img_size = img_size
+        self.stride = stride
+        self.files = images 
+        self.mode = 'image'
+        assert self.nf > 0, f'No images found in {p}. ' \
+
+    def __iter__(self):
+        self.count = 0
+        return self
+
+    def __next__(self):
+        if self.count == self.nf:
+            raise StopIteration
+        path = self.files[self.count]
+        self.count += 1
+        img0 = cv2.imread(path)  # BGR
+        assert img0 is not None, 'Image Not Found ' + path
+
+        # Padded resize
+        img = letterbox(img0, self.img_size, stride=self.stride)[0]
+        # Convert
+        img = img[:, :, ::-1].transpose(2, 0, 1)  # BGR to RGB, to 3x416x416
+        img = np.ascontiguousarray(img)
+
+        return path, img, img0
 
     def __len__(self):
-        return 1 if not self.is_dir else len(self.image_paths)
+        return self.nf  # number of files
 
-    def __getitem__(self, idx):
-        if self.is_dir:
-            image = Image.open(self.image_paths[idx])
-        else:
-            image = self.image
-        if self.transform:
-            image = self.transform(image)
-        return image, None
